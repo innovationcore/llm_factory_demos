a1be9d577d9f82483622ee5820aae4e7cd9b63bb
Cody Bumgardner <cody@uky.edu>
2024-05-27 17:35:08 -0400

    boom

--- langchain_fhir_neo4j/langchain_fhir_neo4j.py
+++ langchain_fhir_neo4j/langchain_fhir_neo4j.py
@@ -1,16 +1,20 @@
+#https://github.com/samschifman/RAG_on_FHIR/blob/main/RAG_on_FHIR_with_KG/FHIR_GRAPHS.ipynb
+
 import glob
 import re
 
 from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.vectorstores import Neo4jVector
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 
 from LoraXAPIEmbeddings import LoraXAPIEmbeddings
 
 import json
 from NEO4J_Graph import Graph
 from FHIR_to_graph import resource_to_node, resource_to_edges
+
+from openai import OpenAI
 
 with open('../config.json') as user_file:
     config = json.load(user_file)
@@ -22,10 +26,15 @@ llm_api_base_local = config['llm_api_base_local']
 llm = ChatOpenAI(
     model_name="",
     openai_api_key=llm_api_key,
-    openai_api_base=llm_api_base_local,
+    openai_api_base=llm_api_base,
     verbose=True,
     streaming=False
 )
+
+client = OpenAI(
+        api_key=llm_api_key,
+        base_url=llm_api_base,
+    )
 
 '''
 llm = ChatOpenAI(
@@ -39,11 +48,11 @@ embeddings = LoraXAPIEmbeddings(
     api_url=llm_api_base,
 )
 
-
+'''
 embeddings = OpenAIEmbeddings(
     openai_api_key=config['openai_api_key']
 )
-
+'''
 
 def get_database():
 
@@ -64,7 +73,8 @@ def load_database(graph):
     graph.wipe_database()
 
     # load database
-    synthea_bundles = glob.glob("working/bundles/*.json")
+    #synthea_bundles = glob.glob("working/bundles/*.json")
+    synthea_bundles = glob.glob('/Users/cody/Downloads/synthea_sample_data_fhir_latest/*.json')
     synthea_bundles.sort()
 
     nodes = []
@@ -117,6 +127,81 @@ def load_database(graph):
         except:
             print(f'Failed to create edge: {edge}')
 
+
+def date_for_question(question_to_find_date, model=""):
+
+
+    system_content = f'''
+    system:Given the following question from the user, extract the date the question is asking about.
+    Return the answer formatted as JSON only, as a single line.
+    Use the form:
+
+    {{"date":"[THE DATE IN THE QUESTION]"}}
+
+    Use the date format of month/day/year.
+    Use two digits for the month and day.
+    Use four digits for the year.
+    So 3/4/23 should be returned as {{"date":"03/04/2023"}}.
+    So 04/14/89 should be returned as {{"date":"04/14/1989"}}.
+
+    Please do not include any special formatting characters, like new lines or "\\n".
+    Please do not include the word "json".
+    Please do not include triple quotes.
+
+    If there is no date, do not make one up. 
+    If there is no date return the word "none", like: {{"date":"none"}}
+    '''
+
+    resp = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": system_content,
+            },
+            {"role": "user",
+             "content": question_to_find_date},
+        ]
+    )
+
+    date_json = None
+    try:
+        date_json = json.loads(resp.choices[0].message.content)['date']
+    except:
+        None
+
+    return date_json
+
+
+def create_contextualized_vectorstore_with_date(date_to_look_for):
+    if date_to_look_for == 'none':
+        contextualize_query_with_date = """
+        match (node)<-[]->(sc:resource)
+        with node.text as self, reduce(s="", item in collect(distinct sc.text) | s + "\n\nSecondary Entry:\n" + item ) as ctxt, score, {} as metadata limit 1
+        return "Primary Entry:\n" + self + ctxt as text, score, metadata
+        """
+    else:
+        contextualize_query_with_date = f"""
+        match (node)<-[]->(sc:resource)
+        where exists {{
+             (node)-[]->(d:Date {{id: '{date_to_look_for}'}})
+        }}
+        with node.text as self, reduce(s="", item in collect(distinct sc.text) | s + "\n\nSecondary Entry:\n" + item ) as ctxt, score, {{}} as metadata limit 1
+        return "Primary Entry:\n" + self + ctxt as text, score, metadata
+        """
+
+    _contextualized_vectorstore_with_date = Neo4jVector.from_existing_index(
+        embeddings,
+        url=config['NEO4J_URI'],
+        username=config['NEO4J_USERNAME'],
+        password=config['NEO4J_PASSWORD'],
+        database=config['NEO4J_DATABASE'],
+        index_name='fhir_text',
+        retrieval_query=contextualize_query_with_date,
+    )
+
+    return _contextualized_vectorstore_with_date
+
 def create_vectors():
 
 
@@ -163,8 +248,6 @@ def create_context_vector_index():
 
     return contextualized_vectorstore
 
-
-
 if __name__ == '__main__':
 
     #connect to neo4j
@@ -173,11 +256,9 @@ if __name__ == '__main__':
     #populate the database
     print('Loading database')
     #load_database(graph)
-    print('Creating vectors')
-    #create_vectors()
-    print('Creating index from vectors')
-    #vector_index = create_vector_index()
-    vector_index = create_context_vector_index()
+    #exit(0)
+    #print('Creating vectors')
+    create_vectors()
 
 
     #prompts
@@ -258,20 +339,43 @@ if __name__ == '__main__':
     # question = "How much did the colon scan eighteen days after the first of the year 2019 cost?"
     #question = "How much did the colon scan on Jan. 18, 2014 cost?"
     #question = "Did Alfonzo975 get a chest x-ray?"
-    question = "What is the patient name of Alfonzo975?"
+    #question = "What is heart rate of patient Alfonzo975 on 03/27/2015."
+    #question = "Tell me about heart "
+    question = "What is the average heart rate?"
     '''
     response = vector_index.similarity_search(question,k=1)  # k_nearest is not used here because we don't have a retrieval query yet.
     print(response)
     print(response[0].page_content)
     print(len(response))
     '''
-
+    '''
     vector_qa = RetrievalQA.from_chain_type(
         llm=llm, chain_type="stuff",
-        retriever=vector_index.as_retriever(search_kwargs={'k': 1}),
+        retriever=vector_index.as_retriever(search_kwargs={'k': k_nearest}),
         # k_nearest is not used here because we don't have a retrieval query yet.
         verbose=True, chain_type_kwargs={"verbose": True, "prompt": prompt},
     )
+    print(vector_qa.run(question))
+    '''
+    print('Creating index from vectors')
+
+    date_str = date_for_question(question)
+    if date_str is not None:
+        print('index with date')
+        vector_index = create_contextualized_vectorstore_with_date(date_str)
+    else:
+        print('index w/o date')
+        #vector_index = create_vector_index()
+        vector_index = create_context_vector_index()
+
+
+    vector_qa = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff",
+        retriever=vector_index.as_retriever(search_kwargs={'k': k_nearest}),
+        # k_nearest is not used here because we don't have a retrieval query yet.
+        verbose=True, chain_type_kwargs={"verbose": True, "prompt": prompt},
+    )
+
     print(vector_qa.run(question))
 
 
