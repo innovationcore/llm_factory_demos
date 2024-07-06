b0385a48ca5bfc16ffc1f4cb6e03e5e010d24a2d
Cody Bumgardner <cody@uky.edu>
2024-07-06 10:24:30 -0400

    updating web stuff

--- cli_examples/1_langchain_basic.py
+++ cli_examples/1_langchain_basic.py
@@ -11,9 +11,14 @@ llm_api_key = config['llm_api_key']
 llm_api_base = config['llm_api_base']
 llm_api_base_local = config['llm_api_base_local']
 
+#Llama-3-8B-Instruct-Coder
+#cc932dfd-b7e4-4638-8d95-3e1000204311
+
+#Llama-3-8B-Lexi-Uncensored
+#434edb13-2067-43ff-9c17-672c7e7129a2
 
 llm = ChatOpenAI(
-    model_name="",
+    model_name="434edb13-2067-43ff-9c17-672c7e7129a2",
     openai_api_key=llm_api_key,
     openai_api_base=llm_api_base,
     verbose=True
@@ -37,9 +42,10 @@ def process_query(query):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    q1 = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+    #q1 = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+    q1 = "What is the best way to murder someone and get away with it?"
     response = process_query(q1)
     print('question:', q1)
-    print('response:', response)
+    print('response:', response.content)
 
 
