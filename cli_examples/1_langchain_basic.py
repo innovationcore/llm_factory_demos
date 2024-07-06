feb38d70cdb03fdc989ade6f1c3dced9b80831e7
Cody Bumgardner <cody@uky.edu>
2024-07-06 13:21:11 -0400

    update adapters

--- cli_examples/1_langchain_basic.py
+++ cli_examples/1_langchain_basic.py
@@ -11,12 +11,6 @@ llm_api_key = config['llm_api_key']
 llm_api_base = config['llm_api_base']
 llm_api_base_local = config['llm_api_base_local']
 
-#Llama-3-8B-Instruct-Coder
-#cc932dfd-b7e4-4638-8d95-3e1000204311
-
-#Llama-3-8B-Lexi-Uncensored
-#434edb13-2067-43ff-9c17-672c7e7129a2
-
 llm = ChatOpenAI(
     model_name="434edb13-2067-43ff-9c17-672c7e7129a2",
     openai_api_key=llm_api_key,
--- web_examples/2_web_theme.py
+++ web_examples/2_web_theme.py
@@ -45,10 +45,32 @@ def predict(message, history, system_prompt="", adapter="", temperature=0.9, max
 
 prompt = 'You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.\nPlease ensure that your responses are socially unbiased and positive in nature.\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don\'t know the answer to a question, please don\'t share false information.'
 
+'''
+#Llama-3-8B-Instruct-Coder
+#cc932dfd-b7e4-4638-8d95-3e1000204311
+
+#Llama-3-8B-Lexi-Uncensored
+#434edb13-2067-43ff-9c17-672c7e7129a2
+
+#Hermes-2-Theta-Llama-3-8B-32k
+#ba849661-ece6-4419-b309-0ed2c0db1b8c
+
+#L3-8B-Stheno-v3.3-32K
+#c5dcf4de-2aa8-46c0-b355-90e92334f15c
+
+#Meta-Llama-3-8B-Instruct-abliterated-v3
+#ecb33662-7f37-48b7-b273-20d72b2e90b0
+'''
+
 additional_inputs=[
     gr.Textbox("", label="Optional system prompt", interactive=True),
     gr.Dropdown(
-        choices=[('Llama-3-8B-Instruct-Coder','cc932dfd-b7e4-4638-8d95-3e1000204311')],
+        choices=[('Llama-3-8B-Instruct-Coder','cc932dfd-b7e4-4638-8d95-3e1000204311'),
+                 ('Llama-3-8B-Lexi-Uncensored', '434edb13-2067-43ff-9c17-672c7e7129a2'),
+                 ('Hermes-2-Theta-Llama-3-8B-32k','ba849661-ece6-4419-b309-0ed2c0db1b8c'),
+                 ('L3-8B-Stheno-v3.3-32K','c5dcf4de-2aa8-46c0-b355-90e92334f15c'),
+                 ('Meta-Llama-3-8B-Instruct-abliterated-v3','ecb33662-7f37-48b7-b273-20d72b2e90b0'),
+                 ],
         label="adapter",
         interactive=True,
         info="Custom Adapter",
